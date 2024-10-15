0fa5274171a0cd70087b386da297504538ba49bb
uchenna23 <uchennanwagbara23@gmail.com>
2024-10-15 17:20:45 -0400

    initial commit

---
+++ src/main/java/com/example/EcoTrade/EcoTradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcoTrade/EcoTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
